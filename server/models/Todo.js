98581189ea6d77b7dbb6c0ec0d8f83c6e0bfdd33
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-03-31 18:32:28 +0530

    first commit

---
+++ server/models/Todo.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+export default Todo;
